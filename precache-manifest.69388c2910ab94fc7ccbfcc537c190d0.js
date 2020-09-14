0c66d3180590b83a584a747de1fd34112d632f71
iganbold <13912728+iganbold@users.noreply.github.com>
2020-09-14 06:28:33 +0000

    Deploying to gh-pages from  @ 452844f59885040d0a7a1eeca3707cf722a9bd5f 🚀

---
+++ precache-manifest.69388c2910ab94fc7ccbfcc537c190d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73168511a0373b8a5d083bb5a0286c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80b3b387254355c396a4",
+    "url": "/static/js/2.87ce6789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87ce6789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1459a5657def7b4eed4",
+    "url": "/static/js/main.397842f9.chunk.js"
+  },
+  {
+    "revision": "2d4092c348932244a15d",
+    "url": "/static/js/runtime-main.94d99c57.js"
+  }
+]);
