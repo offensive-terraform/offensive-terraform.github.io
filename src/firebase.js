abf76100fab9fc3e22201a9a364a5c984a6e49b6
iganbold <itgel.ganbold1@gmail.com>
2020-09-15 00:15:29 -0700

    Import only required modules from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGR1t04Vea4k_51vqBbqWRMUYLZeJgy8o",
+  authDomain: "offensive-terraform.firebaseapp.com",
   databaseURL: "https://offensive-terraform.firebaseio.com",
   projectId: "offensive-terraform",
+  storageBucket: "offensive-terraform.appspot.com",
   messagingSenderId: "556600256441",
   appId: "1:556600256441:web:6a17fe02846490f8b87868",
   measurementId: "G-DEWDK2DB9R",
